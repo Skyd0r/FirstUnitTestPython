83c17a3ef042a52c9e4cbc3efdf5e105c4894cc0
romain <romain.poitrenaud@gmail.com>
2023-12-20 14:49:25 +0100

    bonjour add

--- unitTestPython.py
+++ unitTestPython.py
@@ -1,8 +1,23 @@
+import datetime
+
+now = datetime.datetime.now()
+
+if (now.hour > 18):
+    print("Bonsoir !")
+else:
+    print("Bonjour !")
+
 chaine = input("Rentrez un mot : ")
 
 reverseChaine = "".join(reversed(chaine))
+
 
 if (chaine == reverseChaine):
     print(reverseChaine + "\nbien dit !")
 else:
     print(reverseChaine)
+
+if (now.hour > 18):
+    print("Bonne soirée !")
+else:
+    print("Bonne Journée !")
